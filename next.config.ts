75fdadb31ec3ad4d5dda37b859e6b6b3b6aa3d8a
yaniv.choen.1 <yaniv.choen.1@gmail.com>
2026-02-02 12:40:28 +0200

    explicitly set the turbopack.root to the project root directory using an absolute path

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   /* config options here */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
